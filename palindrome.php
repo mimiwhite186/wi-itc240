9fd57f4a742e1c885c1abb374482ba897e639187
mimiwhite186 <52567953+mimiwhite186@users.noreply.github.com>
2020-02-22 20:30:03 -0800

    Add files via upload

---
+++ palindrome.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = "dog";
+
+$b =  strrev($a);
+
+    $string = str_split($b);
+
+    $palin = '';
+
+    foreach($string as $value){
+
+        $palin.= $value; 
+    }
+
+    if($a == $palin){
+
+        print "The word '{$a}' is palindrome. <br>True";
+
+    } else {
+
+        print "The word '{$a}' is not palindrome.<br>False"; 
+
+    }
+?>
